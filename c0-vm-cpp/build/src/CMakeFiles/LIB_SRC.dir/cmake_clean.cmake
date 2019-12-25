1390b2d7b8c6c42ae568615cf344072d0aaeadd7
nwzhr <807831257@qq.com>
2019-12-25 08:46:29 +0000

    new

---
+++ c0-vm-cpp/build/src/CMakeFiles/LIB_SRC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LIB_SRC.dir/file.cpp.o"
+  "CMakeFiles/LIB_SRC.dir/vm.cpp.o"
+  "libLIB_SRC.pdb"
+  "libLIB_SRC.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LIB_SRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c0-vm-cpp/build/src/CMakeFiles/c0-vm-cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c0-vm-cpp.dir/main.cpp.o"
+  "c0-vm-cpp.pdb"
+  "c0-vm-cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c0-vm-cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
